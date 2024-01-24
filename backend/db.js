1dc3d2e76296169478d34b3271317d53193a1f2f
raja <marwah@MacBook-Air-de-Marwah.local>
2024-01-24 10:42:11 +0100

    re

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+import pg from 'pg';
+const {Pool} = pg;
+
+let localPool = {
+    user: 'postgres',
+    host:'localhost',
+    port: '5432',
+    database: 'testtechn'
+};
+
+const poolConfig = process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL, 
+    ssl:{rejectUnauthorized:false}
+} : localPool;
+
+const pool = new Pool(poolConfig);
+export default pool;
+
